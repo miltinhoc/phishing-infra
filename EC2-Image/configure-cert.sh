03a916babc5a883022a55d09298ed42131c67af8
miltinhoc <miltinhoc@users.noreply.github.com>
2023-09-28 20:10:50 +0000

    Create configure-cert.sh
    this script will be used to configure/create the ssl certificate from letsencrypt and modify gophish settings

---
+++ EC2-Image/configure-cert.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [[ -z "$1" ]]; then
+    echo "[*] error: no domain provided"
+    exit 1
+fi
+
+DOMAIN="$1"
+KEY="/etc/letsencrypt/live/$DOMAIN/privkey.pem"
+CERT="/etc/letsencrypt/live/$DOMAIN/fullchain.pem"
+
+sudo certbot certonly --standalone -d $DOMAIN -d www.$DOMAIN
+
+# Check for the certificate and private key
+if [[ -f "$CERT" && -f "$KEY" ]]; then
+    echo "[*] certificate and private key generated with success!"
+
+    echo "[*] changing gophish config..."
+
+    sed -i "s|:80|:443|g" config.json
+    sed -i "s|gophish_admin.crt|$CERT|g" config.json
+    sed -i "s|gophish_admin.key|$KEY|g" config.json
+    sed -i "s|example\.crt|$CERT|g" config.json
+    sed -i "s|example\.key|$KEY|g" config.json
+
+    ./gophish
+else
+    echo "[*] error: couldn't generate certificate and private key."
+    exit 1
+fi
